bfbcb011e0d78f1ddc91e474d26ac9033136b46e
Vitor Silva <vitorsilvati2019@hotmail.com>
2022-09-13 11:07:53 -0300

    update 13/09/2022

--- venv/project-files/visualizador_de_dados.py
+++ venv/project-files/visualizador_de_dados.py
@@ -4,7 +4,7 @@ import matplotlib.pyplot as plt
 def visualizaInfo():
     plt.figure(figsize=(6,6))
     plt.plot(cotacao_empresa['Close'])
-    plt.xticks(range(0,cotacao_empresa.shape[0],100), cotacao_empresa['Date'].loc[::100], rotation=35)
+    #plt.xticks(range(0,cotacao_empresa.shape[0],100), cotacao_empresa['Date'].loc[::100], rotation=35)
     plt.xlabel('Data', fontsize=15)
     plt.ylabel('Preço Fechamento', fontsize=18)
     plt.title('Histórico Fechamento PETR4.SA', fontsize=30)
